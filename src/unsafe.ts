0f4d21d16b1b13959ec913cf460add4802d638df
Kirtik <128562483+seducked@users.noreply.github.com>
2026-09-15 19:33:54 +0800

    Clean up comments in unsafe.ts
    Removed unnecessary comments and added a new comment for testing purposes.

--- src/unsafe.ts
+++ src/unsafe.ts
@@ -13,3 +13,4 @@ export function handle(req: { body: string }) {
 
 
 //this is a Epherem test done on 15/9/2026 1519 to test scan upon push. tung tung sahur. 
+//test, 15/9/2026 1933. if you see me then you know me then you know i wanna go. fast and furiousssssssssssssssssssssssss eh eh eh fast and furioussssss eh eh
